4a2ed32dbc78cfd081d0cae81322c55b0024ff2b
Lucas Vieira <lrvieira@ucs.br>
2021-04-17 12:28:23 -0300

    First working example

--- tsp.cpp
+++ tsp.cpp
@@ -17,34 +17,26 @@ public:
         m_geneSize = genSize;
     }
 
-    void setNumberOfGenerations(int numGen)
-    {
-        m_numberOfGenerations = numGen;
-    }
-
-    void setNumberOfTournments(int numTourn)
-    {
-        m_numberOfTournments = numTourn;
-    }
-
-    void setCrossoverProbability(double crossProb)
-    {
-        m_crossoverProbability = crossProb;
-    }
 
     void setMutationProbability(double mutProb)
     {
         m_mutationProbability = mutProb;
     }
 
+    void showIndividual(std::vector<int> &indiv)
+    {
+        for (int i = 0; i < indiv.size(); i++)
+        {
+            std::cout << indiv.at(i) << " ";
+        }
+
+        //std::cout << std::endl;
+    }
 
 private:
     int     m_geneSize;
     int     m_populationSize;
-    int     m_numberOfTournments;
-    int     m_numberOfGenerations;
     double  m_mutationProbability;
-    double  m_crossoverProbability;
 
     std::vector<std::vector<int>>   population;
     std::vector<int>                individual;
@@ -121,46 +113,76 @@ public:
 
     void crossoverTournment(std::vector<std::vector<int>> &population, int fatherIndex, int motherIndex, std::vector<int> &child1, std::vector<int> &child2)
     {
-        int point1 = std::rand() % m_geneSize;
-        int point2;
-        do
-        {
-            point2 = std::rand() % m_geneSize;
-        } while (point1 == point2);
+        int crossoverPoint = std::rand() % m_geneSize;
+        std::vector<int> auxChild1, auxChild2;
+        
+        child1.insert(child1.end(), population[fatherIndex].begin(), population[fatherIndex].begin()+crossoverPoint);
+        child1.insert(child1.end(), population[motherIndex].begin()+crossoverPoint, population[motherIndex].end());
 
-        int p1 = std::min(point1, point2);
-        int p2 = std::max(point1, point2);
-        std::cout << p1 << " " << p2 << std::endl;
-                
-        for (int i = 0; i < p1; i++)
-        {
-            child1.push_back(population[fatherIndex][i]);
-            child2.push_back(population[motherIndex][i]);
-        }
+        child2.insert(child2.end(), population[motherIndex].begin(), population[motherIndex].begin()+crossoverPoint);
+        child2.insert(child2.end(), population[fatherIndex].begin()+crossoverPoint, population[fatherIndex].end());
 
-        for (int i = p1; i < p2; i++)
+        auxChild1 = population[motherIndex];
+        auxChild2 = population[fatherIndex];
+        
+        for (int i = 0; i < m_geneSize; i++)
         {
-            auto it = std::find(population[motherIndex].begin(), population[motherIndex].end(), population[fatherIndex][i]);
-            
-            if (std::distance(population[motherIndex].begin(), it) >= p1 && std::distance(population[motherIndex].begin(), it) < p2)
-            {
-                //std::cout << "encontrou no indice " << std::distance(population[motherIndex].begin(), it) << std::endl;
-                child1.push_back(population[fatherIndex][i]);
-                child2.push_back(population[motherIndex][i]);
-            }
-            else
-            {
-                child1.push_back(population[motherIndex][i]);
-                child2.push_back(population[fatherIndex][i]);
-            }
-            
+            auxChild1.erase(std::remove(auxChild1.begin(), auxChild1.end(), child1[i]), auxChild1.end());
+            auxChild2.erase(std::remove(auxChild2.begin(), auxChild2.end(), child2[i]), auxChild2.end());
         }
         
-        for (int i = p2; i < m_geneSize; i++)
+        auto end = child1.end();
+        for (auto it = child1.begin(); it != end; ++it)
+            end = std::remove(it + 1, end, *it);
+
+        child1.erase(end, child1.end());
+
+        end = child2.end();
+        for (auto it = child2.begin(); it != end; ++it)
+            end = std::remove(it + 1, end, *it);
+
+        child2.erase(end, child2.end());
+
+        child1.insert(child1.end(), auxChild1.begin(), auxChild1.end());
+        child2.insert(child2.end(), auxChild2.begin(), auxChild2.end());
+    }
+
+    void mutation(std::vector<int> &indiv)
+    {
+        int gene1 = rand() % m_geneSize;
+        int gene2;
+
+        do
         {
-            child1.push_back(population[fatherIndex][i]);
-            child2.push_back(population[motherIndex][i]);
+            gene2 = rand() % m_geneSize;
+        } while (gene1 == gene2);
+
+        std::cout << gene1 << " " << gene2 << std::endl;
+        
+        int auxGene;
+
+        auxGene = indiv[gene1];
+
+        indiv[gene1] = indiv[gene2];
+        indiv[gene2] = auxGene;
+    }
+
+    int getFittest(std::vector<std::vector<int>> &population, std::vector<std::vector<double>> &citiesPosition)
+    {
+        int indexOfBest = 0;
+        int scoreOfBest = scoreOfIndividual(population[indexOfBest], citiesPosition);
+
+        for (int i = 1; i < m_populationSize; i++)
+        {
+            int score = scoreOfIndividual(population[i], citiesPosition);
+
+            if (score < scoreOfBest)
+            {
+                indexOfBest = i;
+                scoreOfBest = score;
+            }
         }
+        return indexOfBest;
     }
 
 };
@@ -171,39 +193,87 @@ int main(){
 
     std::vector<std::vector<double>> cities = 
     {
-        {15.5 ,  5.7},
-        { 3.8 , 10.9},
-        { 7.2 ,  3.4},
-        {13.1 , 12.4},
-        { 5.1 , 11.9},
-        { 9.9 ,  4.8},
-        { 2.7 ,  9.4}
+        {  8.660 ,  5.000},
+        {  5.000 ,  8.660},
+        {  0.000 , 10.000},
+        { -5.000 ,  8.660},
+        { -8.660 ,  5.000},
+        {-10.000 ,  0.000},
+        { -8.660 , -5.000},
+        { -5.000 , -8.660},
+        {  0.000 ,-10.000},
+        {  5.000 , -8.660},
+        {  8.660 , -5.000},
+        { 10.000 ,  0.000}
     };
 
-    TSP trip;
-    trip.setPopulationSize(10);
-    trip.setGeneSize(cities.size());
-    trip.setNumberOfGenerations(100);
-    trip.setNumberOfTournments(20);
-    trip.setCrossoverProbability(0.7);
-    trip.setMutationProbability(0.1);
+    double aim = 63;
 
+    int     numberOfGenerations = 200;
+    int     numberOfTournments = 400;
+    int     populationSize = 1000;
+    int     geneSize = cities.size();
+    double  crossoverProbability = 0.7;
+    double  mutationProbability = 0.1;
+
+    TSP trip;
+    trip.setPopulationSize(populationSize);
+    trip.setGeneSize(geneSize);
+    trip.setMutationProbability(mutationProbability);
 
     std::vector<std::vector<int>> routes;
     routes = trip.initializePopulation();
 
-    trip.showPopulation();
+   for (int i = 0; i < numberOfGenerations; i++)
+   {
+       for (int j = 0; j < numberOfTournments; j++)
+       {
+           double probability = ((double) std::rand()/ ((double)RAND_MAX + 1));
 
-    std::cout << trip.scoreOfIndividual(routes[1], cities) << std::endl;
+            if (probability > crossoverProbability)
+            {
+                int fatherIndex = std::rand() % populationSize;
+                int motherIndex;
 
-    std::vector<int> firstChild, secondChild;
-    trip.crossoverTournment(routes, 0, 1, firstChild, secondChild);
+                do
+                {
+                    motherIndex = std::rand() % populationSize;
+                } while (fatherIndex == motherIndex);
+                
+                std::vector<int> firstChild, secondChild;
 
-    for (int i = 0; i < cities.size(); i++)
-    {
-        std::cout << firstChild[i] << " ";
-    }
-    std::cout << std::endl;
-    
+                trip.crossoverTournment(routes, fatherIndex, motherIndex, firstChild, secondChild);
 
+                double scoreFather =       trip.scoreOfIndividual(routes[fatherIndex], cities);
+                double scoreMother =       trip.scoreOfIndividual(routes[motherIndex], cities);
+                double scoreFirstChild =   trip.scoreOfIndividual(firstChild, cities);
+                double scoreSecondChild =  trip.scoreOfIndividual(secondChild, cities);
+
+                std::vector<int> fit1, fit2, nonFit1, nonFit2;
+                
+                if (scoreFirstChild + scoreSecondChild < scoreFather + scoreMother)
+                {
+                    for (int k = 0; k < geneSize; k++)
+                    {
+                        routes[fatherIndex][k] = firstChild[k];
+                        routes[motherIndex][k] = secondChild[k];
+                    }
+                }
+            }
+       } 
+
+        std::cout << "Generation: " << i+1;
+        std::cout << " | Best: ";
+
+        int indexOfBest = trip.getFittest(routes, cities);
+        double scoreOfBest = trip.scoreOfIndividual(routes[indexOfBest], cities);
+        trip.showIndividual(routes[indexOfBest]);
+
+        std::cout << " | Score: " << scoreOfBest << "\n\n";
+
+        if (scoreOfBest <= aim)
+        {
+            break;
+        }
+   }
 }
